e8f443fb3c30c5df6b12fdc1950ea9876b80af05
Hyun <hyun_7586@naver.com>
2025-05-03 00:57:57 +0900

    Level3 제네릭 기능 추가

--- src/main/java/org/example/calculator/calculatorLV3/ArithmeticCalculator.java
+++ src/main/java/org/example/calculator/calculatorLV3/ArithmeticCalculator.java
@@ -4,25 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class ArithmeticCalculator {
+public class ArithmeticCalculator<T> {
 
-  private List<Integer> resultList = new ArrayList<>();
+  private List<T> resultList = new ArrayList<>();
 
-  public Optional<Integer> calculate(int a, int b, char operator) {
+  public Optional<Double> calculate(T a, T b, char operator) {
     OperatorType op = OperatorType.fromSymbol(operator);
-
     if(op==null)  return Optional.empty();
 
-    return op.operate(a, b);
+    if(a instanceof Number && b instanceof Number)  return op.operate((double)a, (double)b);
+
+    return Optional.empty();
   }
 
   // getter
-  public List<Integer> getResultList() {
+  public List<T> getResultList() {
     return resultList;
   }
 
   // setter
-  public void setResultList(List<Integer> resultList) {
+  public void setResultList(List<T> resultList) {
     this.resultList=resultList;
   }
 
